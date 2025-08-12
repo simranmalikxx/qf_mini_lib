b6e2b1ab7d6b54f0b30895e7e3b665015856b2f0
simranmalikxx <155261360+simranmalikxx@users.noreply.github.com>
2025-08-12 06:09:16 -0700

    updates

--- utils/enum.py
+++ utils/enum.py
@@ -5,17 +5,60 @@ Created on Mon Aug 11 11:16:23 2025
 @author: Simran
 """
 
-from enum import Enum, auto 
+from enum import Enum
 
 class OptionType(Enum):
-    CALL = auto()
-    PUT = auto()
+    CALL = 1
+    PUT = 2
     
 class ExerciseType(Enum):
-    AMERICAN = auto()
-    EUROPEAN = auto()
+    AMERICAN = 1
+    EUROPEAN = 2
 
 class ValuationType(Enum):
-    ANALYTICAL = auto()
-    MONTE_CARLO = auto()
-    FINITE_DIFFERENCE = auto()
+    ANALYTICAL = 1
+    MONTE_CARLO = 2
+    FINITE_DIFFERENCE = 3
+    
+class MonteCarloMethod(Enum):
+    STANDARD = 1
+    ANTITHETIC = 2
+    CONTROL_VARIATE = 3
+    QUASI_RANDOM = 4
+
+class PDEScheme(Enum):
+    EXPLICIT = 1
+    IMPLICIT = 2
+    CRANK_NICOLSON = 3
+
+    
+# Asian Speciifc: 
+class AsianStrikeType(Enum):
+    FIXED = 1
+    FLOATING = 2
+
+class AsianAveragingType(Enum):
+    ARITHMETIC = 1
+    GEOMETRIC = 2
+    
+class AsianAnalyticalMethod(Enum):
+    GEOMETRIC_MEAN = 1
+    TURNBULL_WAKEMAN = 2
+    CURRAN = 3
+
+
+# Barrier Specific: 
+class BarrierType(Enum):
+    UP_IN = 1
+    UP_OUT = 2
+    DOWN_IN = 3
+    DOWN_OUT = 4
+
+class BarrierAnalyticalMethod(Enum):
+    REINHARDT = 1
+    MERTON = 2
+    BARRIERSHIFT = 3
+
+class RebateType(Enum):
+    FIXED = 1         
+    PERCENTAGE = 2    
--- utils/global.py
+++ utils/global.py
@@ -8,7 +8,14 @@ Created on Mon Aug 11 12:02:45 2025
 
 # Common day count constants
 days_in_year = 365.0
+DAYS_IN_LEAP_YEAR = 366.0
 days_in_month = 30.0
-
-# Constants for interest rate calculations or market conventions
 trading_days_per_year = 252
+
+DEFAULT_NUM_PATHS = 10000
+FD_DEFAULT_STEPS_TIME = 100
+FD_DEFAULT_STEPS_SPACE = 200
+
+
+DEFAULT_DECIMALS = 6
+EPSILON = 1e-8
